b273ca40daf8bfa8bd979818856f2253b3bee9e7
Rickard Jonsson <rickaard@gmail.com>
2020-05-08 01:10:36 +0200

    Started to implement socket.io

--- server/index.js
+++ server/index.js
@@ -6,17 +6,22 @@ const cors = require('cors');
 const http = require('http');
 const shortId = require('shortid');
 
+const router = require('./router');
+
 // SETUP ENV VARIABELS
 require('dotenv').config();
 
 // CONFIGURE EXPRESS APP
 const app = express();
-app.use(express.json());
-app.use(cors());
+
 
 const server = http.createServer(app);
-
 const io = socketIO(server);
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
 
 // UTILS
 const poolDivider = require('./utils/utils');
@@ -24,11 +29,7 @@ const poolDivider = require('./utils/utils');
 // Connect to DB
 mongoose.connect(
     process.env.MONGODB_URL,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    },
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
     () => console.log('Connected to DB')
 );
 
@@ -36,72 +37,37 @@ mongoose.connect(
 const Tournament = require('./models/Tournament');
 
 
-app.get('/', (req, res) => {
-    console.log(req);
-    res.send({ message: 'yey' });
-})
+//
+let interval;
+io.on('connect', (socket) => {
+    console.log('We have a new connection');
 
+    socket.on('join', ({ tournamentId }, callback) => {
 
+        Tournament.findById(tournamentId, (error, tournament) => {
+            if (error) return callback(error); // if no tournament is found, call callback with argument error and return from function
 
-app.post('/create-tournament', async (req, res) => {
+            socket.emit('tournament', tournament); // emit the tornament object that is fetched from DB
+            socket.join(tournamentId); // join tournament "room"
+        });
 
-    // FIXA VALIDERING!!! @HAPI/JOI???
-
-    const { tournamentName, teams, email } = req.body;
-    console.log(email)
-    console.log(teams);
-
-    // shuffle teams array and divide into first pool
-    const gamesArray = poolDivider(teams);
-    const generatedId = shortId.generate();
-    console.log('genererad auth id: ', generatedId);
-
-    // Create a new tournament object
-    const tournament = new Tournament({
-        tournamentName,
-        authId: generatedId,
-        teams: teams.map(team => ({ name: team, wins: 0, losses: 0 })),
-        pools: [
-            {
-                round: 1,
-                wins: 0,
-                losses: 0,
-                poolType: '0-0',
-                games: gamesArray
-            },
-            { round: 2, wins: 1, losses: 0, poolType: '1-0', games: [] },
-            { round: 2, wins: 0, losses: 1, poolType: '0-1', games: [] },
-            { round: 3, wins: 2, losses: 0, poolType: '2-0', games: [] },
-            { round: 3, wins: 1, losses: 1, poolType: '1-1', games: [] },
-            { round: 3, wins: 0, losses: 2, poolType: '0-2', games: [] },
-            { round: 4, wins: 2, losses: 1, poolType: '2-1', games: [] },
-            { round: 4, wins: 1, losses: 2, poolType: '1-2', games: [] },
-            { round: 5, wins: 2, losses: 2, poolType: '2-2', games: [] },
-        ]
-    });
-
-    // res.send({tournament, email});
-
-    // Save to DB
-    try {
-        const savedTournament = await tournament.save();
-        console.log('NY TURNERING SPARAD TILL DATABAS!');
-        res.send({ savedTournament });
-    } catch (error) {
-        res.status(400).send(error);
-    }
-})
-
-
-app.get('/tournament/:id', (req, res) => {
-    console.log('Client requested a tournament document');
-
-    Tournament.findById(req.params.id, (error, tournament) => {
-        if (error) return res.status(500).send(error);
-        return res.status(200).send({ tournament });
     })
-})
 
+    socket.on('update-match', (data, callback) => {
+        const { teams, pools, matchResult, tournamentId } = data.tournamentObj;
+
+        Tournament.findByIdAndUpdate(tournamentId, { teams: teams, pools: pools }, { new: true }, (error, tournament) => {
+            if (error) return callback(error);
+
+            console.log('DB uppdaterad!');
+            // emit to everyone in the same "room" except the socket client that is active
+            socket.broadcast.to(tournamentId).emit('match-updated', { tournament, matchResult });
+            callback();
+        })
+    })
+
+
+})
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ server/router.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const shortId = require('shortid');
+
+const Tournament = require('./models/Tournament');
+const poolDivider = require('./utils/utils');
+
+router.get('/', (req, res) => {
+    res.send({ message: 'yey' });
+});
+
+router.post('/create-tournament', async (req, res) => {
+
+    // FIXA VALIDERING!!! @HAPI/JOI???
+    // FIXA MEJL-UTSKICK
+
+    const { tournamentName, teams, email } = req.body;
+    // console.log(email)
+    // console.log(teams);
+
+    // shuffle teams array and divide into first pool
+    const gamesArray = poolDivider(teams);
+    const generatedId = shortId.generate();
+    console.log('genererad auth id: ', generatedId);
+
+    // Create a new tournament object
+    const tournament = new Tournament({
+        tournamentName,
+        authId: generatedId,
+        teams: teams.map(team => ({ name: team, wins: 0, losses: 0 })),
+        pools: [
+            {
+                round: 1,
+                wins: 0,
+                losses: 0,
+                poolType: '0-0',
+                games: gamesArray
+            },
+            { round: 2, wins: 1, losses: 0, poolType: '1-0', games: [] },
+            { round: 2, wins: 0, losses: 1, poolType: '0-1', games: [] },
+            { round: 3, wins: 2, losses: 0, poolType: '2-0', games: [] },
+            { round: 3, wins: 1, losses: 1, poolType: '1-1', games: [] },
+            { round: 3, wins: 0, losses: 2, poolType: '0-2', games: [] },
+            { round: 4, wins: 2, losses: 1, poolType: '2-1', games: [] },
+            { round: 4, wins: 1, losses: 2, poolType: '1-2', games: [] },
+            { round: 5, wins: 2, losses: 2, poolType: '2-2', games: [] },
+        ]
+    });
+    // Save to DB
+    try {
+        const savedTournament = await tournament.save();
+        console.log('NY TURNERING SPARAD TILL DATABAS!');
+        res.send({ savedTournament });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// router.put('/tournament/:id', (req, res) => {
+//     // console.log('tournamentId: ', req.params.id);
+//     const { id } = req.params;
+//     const { teams, pools, matchResult } = req.body;
+
+//     Tournament.findByIdAndUpdate(id, { teams: teams, pools: pools }, (error, tournament) => {
+//         if (error) return res.status(500).send(error);
+//         console.log('DB uppdaterad!');
+//         return res.status(202).send({ message: 'Tournament updated', status: 'OK', matchResult })
+//     })
+// })
+
+// app.get('/tournament/:id', (req, res) => {
+//     console.log('Client requested a tournament document');
+
+//     Tournament.findById(req.params.id, (error, tournament) => {
+//         if (error) return res.status(500).send(error);
+//         return res.status(200).send({ tournament });
+//     })
+// })
+
+module.exports = router;
